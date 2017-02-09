635d2ae169b6649e24708ca5277108629ca15e31
Nicolò Crisafulli <nicolo.crisafulli@villaggio.org>
2017-02-09 17:02:24 +0100

    Aggiunti file iniziali

---
+++ SecondLesson/src/secondlesson/SecondLesson.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secondlesson;
+
+/**
+ *
+ * @author utente
+ */
+public class SecondLesson {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int var;
+        double x;
+        var = 10;
+        x = 10.0;
+        System.out.println("Original value o var:" + var);
+        System.out.println("Original value o x:" + x);
+        System.out.println();
+        var = var/4;
+        x = x/4;
+        System.out.println("var after division:" + var);
+        System.out.println("x after division:" + x);
+    }
+    
+}
